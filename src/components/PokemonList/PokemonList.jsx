18d28ed73643fe9c331d14ecd1b5d8b83d9b6f0f
Sumit Kumar <bingostudypoint@gmail.com>
2023-08-21 19:18:58 +0530

    add next and prev button and more custom work in react js

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,38 +1,48 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./PokemonList.css";
 import axios from "axios";
 import Pokemon from "../Pokemon/Pokemon";
+
 function PokemonList() {
   const [pokemonList, setPokemonList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [pokedexUrl, setPokedexUrl] = useState('http://pokeapi.co/api/v2/pokemon');
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
 
   async function pokemonData() {
-    const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+    setIsLoading(true);
+    const response = await axios.get(pokedexUrl);
     const PokemonResult = response.data.results;
-    const pokemonPromise = PokemonResult.map((pokemon) =>
-      axios.get(pokemon.url)
-    );
+
+    setNextUrl(response.data.next);
+    setPrevUrl(response.data.previous);
+
+    const pokemonPromise = PokemonResult.map((pokemon) => axios.get(pokemon.url));
+     
     const pokemonData = await axios.all(pokemonPromise);
-    const res = pokemonData.map((pokeData) => {
+
+    const pokemonResponse = pokemonData.map((pokeData) => {
       const pokemon = pokeData.data;
       return {
         id: pokemon.id,
         name: pokemon.name,
         image: pokemon.sprites.other.dream_world.front_default,
-        types: pokemon.types[0].type.name,
+        types: pokemon.types[0].type.name, // Corrected this line
       };
     });
-    setPokemonList(res);
+
+    setPokemonList(pokemonResponse);
     setIsLoading(false);
   }
 
-  useEffect(async () => {
+  useEffect(() => {
     pokemonData();
-  }, []);
+  }, [pokedexUrl]);
 
   return (
     <div className="wrapper-pokemonList">
-      <span>PokemonList</span>
+         <span>PokemonList</span>
       {isLoading
         ? "Loading..."
         : pokemonList.map((p) => (
@@ -43,7 +53,15 @@ function PokemonList() {
             type={p.type}
           />
         ))}
+      
+
+      <div className="controls">
+        <button disabled={prevUrl == null} onClick={() => setPokedexUrl(prevUrl)}>Prev</button>
+        <button disabled={nextUrl == null} onClick={() => setPokedexUrl(nextUrl)}>Next</button>
+      </div>
+
     </div>
   );
 }
+
 export default PokemonList;
